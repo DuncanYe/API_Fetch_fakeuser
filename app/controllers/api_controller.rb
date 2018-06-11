0208cad117f71eca607a0d0a2b6fe651c91b5b58
Duncan <loxa10077@gmail.com>
2018-06-11 09:57:45 +0800

    set users api vi resources

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,10 @@
 class ApiController < ActionController::Base
+
+  # 由於 API 的防禦機制和 Website 不同，在 ApplicationController 
+  # 裡有一行 protect_from_forgery with: :exception，
+  # 這行程式會啟動瀏覽器的安全檢查，但是 API 的請求與回應不會通過瀏覽器，
+  # 因此不需要這樣的機制。
+
+  # 所以建立此類別，注意superclass 需改成 ActionController::Base
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,13 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :api, default: {format: :json} do
+    namespace :v1 do
+      resources :users, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
+
+  # 網址上有兩層 namespace，除了識別用的 /api，api有可能會更新，加上了版號 /v1提供識別用。
+  # defaults: { format: :json } 將回傳給客戶端的格式預設為 JSON。
+
 end
