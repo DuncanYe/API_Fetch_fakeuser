f614eef520c82f1e1a2892dd239e8fb375af234e
Duncan <loxa10077@gmail.com>
2018-06-12 10:14:44 +0800

    authentica user from token

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,12 @@
 class ApiController < ActionController::Base
+  before_action :authenticate_user_from_token!
+
+  def authenticate_user_from_token!
+    if params[:auth_token].present?
+      user = User.find_by_authentication_token(params[:auth_token])
+      sign_in(user, store: false) if user
+    end
+  end
 
   # 由於 API 的防禦機制和 Website 不同，在 ApplicationController 
   # 裡有一行 protect_from_forgery with: :exception，
