966ce4bef4f33dda13d494f8a45a3042357ecc4e
Duncan <32873800+DuncanYe@users.noreply.github.com>
2019-03-26 10:49:24 +0800

    optimize routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,14 +13,15 @@ Rails.application.routes.draw do
       post "/logout" => "auth#logout"
 
       resources :photos, only: [:index, :create, :show, :update, :destroy]
-    end
-  end
-
-  namespace :api, default: {format: :json} do
-    namespace :v1 do
       resources :users, only: [:index, :create, :show, :update, :destroy]
     end
   end
+
+#   namespace :api, default: {format: :json} do
+#     namespace :v1 do
+
+#     end
+#   end
 
   # 網址上有兩層 namespace，除了識別用的 /api以外，因為api有可能會更新，加上了版號 /v1提供識別用。
   # defaults: { format: :json } 將回傳給客戶端的格式預設為 JSON。
